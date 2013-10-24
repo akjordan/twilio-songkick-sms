a2d18fb46dfd320d99f453c33d785f307fbb8cad
Andrew <andrew.k.jordan@gmail.com>
2013-10-23 23:28:35 -0700

    fixes a number of ills and puts proper date and time checking in place

--- app.rb
+++ app.rb
@@ -1,21 +1,15 @@
 require 'sinatra'
 require 'uri'
 
-# setup RestClient caching backed by Memcachier
+#setup RestClient caching backed by Memcachier
 RestClient.enable Rack::Cache,
   :verbose      => true,
   :metastore   => Dalli::Client.new,
   :entitystore => Dalli::Client.new
 
 
-get "/" do
-
-puts "Hi Mom"
-
-end
-
 # this route handles all POST requests from Twilio
- post "/" do
+ get "/" do
 
 # take the body of the SMS and remove any spaces and make all lower case
 incoming_sms = params["Body"].downcase
@@ -23,7 +17,7 @@ incoming_sms = params["Body"].downcase
 #If they text help return a help message
 if incoming_sms.include?("help")
 	# query Splitcast API and store JSON of spots
-	response = Twilio::TwiML::Response.new  { |r| r.Sms "Type in venue name and city to get a venue ID. \"The Warfield SF\" would be a good search. Once you have an ID, enter it to get the next 5 shows at that venue!" }
+	response = Twilio::TwiML::Response.new  { |r| r.Sms "Search for a venue name and city to get a venue ID ie: \"The Warfield SF\". Once you have an ID, enter it to get the next 7 days of shows at that venue!" }
 # if they send in just numbers search for a venue calendar
 elsif incoming_sms.match('^[0-9]+$')
 	response_string = ""
@@ -32,18 +26,19 @@ elsif incoming_sms.match('^[0-9]+$')
 	data = RestClient.get("http://api.songkick.com/api/3.0/venues/#{incoming_sms}/calendar.json?apikey=PxY0ITiYDczYNR9t", :accept => :json)
 	data = JSON.parse(data)
 	data = data["resultsPage"]["results"]["event"]
-
-	z = 0
 	
-	data.each do |i| 
-	
-	while z < 5	
-	  response_string << "#{i["displayName"]}\n" 
-	end
-	z += 1
+	data.each do |i|
+	  unless i["start"]["datetime"].nil?
+	    puts i["start"]["datetime"]
+	    event_date = DateTime.parse(i["start"]["datetime"]).to_date
+	    date_difference = event_date - DateTime.now.to_date
+	    if date_difference > 0 && date_difference <= 7 
+	      response_string << "#{i["displayName"]}\n"
+	    end
+	  end
 	end
 	# build Twilio response
-	response = Twilio::TwiML::Response.new  { |r| r.Sms "Show Name and Time: \n#{response_string}" }
+	response = Twilio::TwiML::Response.new  { |r| r.Sms "Show Details: \n#{response_string}" }
 #otherwise interperate this search for a venue ID
 else
 	response_string = ""
@@ -51,7 +46,6 @@ else
 	incoming_sms_escaped = URI.escape(incoming_sms)
 	data = RestClient.get("http://api.songkick.com/api/3.0/search/venues.json?query=#{incoming_sms_escaped}&apikey=PxY0ITiYDczYNR9t", :accept => :json)
 	data = JSON.parse(data)
-
 	data = data["resultsPage"]["results"]["venue"]
 
 	data.each { |i| response_string << "#{i["displayName"]}: #{i["id"]}\n" }
