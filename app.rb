6ed8f123ad3c41f4b75714e3b6347cd6a35f355f
Andrew <andrew.k.jordan@gmail.com>
2013-10-23 17:17:08 -0700

    Cleans up

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ incoming_sms = params["Body"].downcase
 #If they text help return a help message
 if incoming_sms.include?("help")
 	# query Splitcast API and store JSON of spots
-	response = Twilio::TwiML::Response.new  { |r| r.Sms "Type in venue name and city to get a venue ID \" The Warfield SF\" would be a good search. Once you have an ID enter it to get the next 5 shows at that venue!" }
+	response = Twilio::TwiML::Response.new  { |r| r.Sms "Type in venue name and city to get a venue ID. \"The Warfield SF\" would be a good search. Once you have an ID, enter it to get the next 5 shows at that venue!" }
 # if they send in just numbers search for a venue calendar
 elsif incoming_sms.match('^[0-9]+$')
 	response_string = ""
@@ -27,17 +27,17 @@ elsif incoming_sms.match('^[0-9]+$')
 	data = data["resultsPage"]["results"]["event"]
 
 	z = 0
+	
 	data.each do |i| 
 	
-	if z < 5	
-	response_string << "#{i["displayName"]}: #{i["start"]["time"]}\n" 
+	while z < 5	
+	  response_string << "#{i["displayName"]}\n" 
+	  z += 1
 	end
-
-	z += 1
-
+	
 	end
 	# build Twilio response
-	response = Twilio::TwiML::Response.new  { |r| r.Sms "#{response_string}" }
+	response = Twilio::TwiML::Response.new  { |r| r.Sms "Show Name and Time: \n#{response_string}" }
 #otherwise interperate this search for a venue ID
 else
 	response_string = ""
@@ -45,11 +45,12 @@ else
 	incoming_sms_escaped = URI.escape(incoming_sms)
 	data = RestClient.get("http://api.songkick.com/api/3.0/search/venues.json?query=#{incoming_sms_escaped}&apikey=PxY0ITiYDczYNR9t", :accept => :json)
 	data = JSON.parse(data)
+
 	data = data["resultsPage"]["results"]["venue"]
 
 	data.each { |i| response_string << "#{i["displayName"]}: #{i["id"]}\n" }
 
-	response = Twilio::TwiML::Response.new  { |r| r.Sms "#{response_string}" }
+	response = Twilio::TwiML::Response.new  { |r| r.Sms "Venue and Venue ID:\n#{response_string}" }
 end
 # return valid TwiML back to Twilio
 response.text
